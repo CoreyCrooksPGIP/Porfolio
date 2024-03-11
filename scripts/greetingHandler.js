7009430aa14e4c9df9013b4a2299dc023ef09d5e
Corey Crooks <corey.crooks@pgip-tech.com>
2024-03-11 03:21:48 -0700

    Introduced IMG CSS and Javascript
    -Expanded Home Page to include a temporary icon, a header banner to introduce me, and a main content section with a randomized greeting that occasionally cycles out for a new one.
    
    -Added CSS rules for a boiler-plate universal page reset, and added styles for various elements on the index page.
    
    -Added a JavaScript page to handle the randomized greeting.

---
+++ scripts/greetingHandler.js
@@ -0,0 +1,87 @@
+//Start greeting "type" effect-----------------------------------------------------------------
+
+//Start the helper chain method to have an easy way to modify how this effect starts, and what it looks like going through.
+function newGreeting(elementID)
+{
+    //Start a variable to keep track of the current greeting's index
+    greetingIndex = -1;
+    //Call step one.
+    eraseGreeting(elementID);
+}
+
+//Start method to gradually delete the text in the element as specified by the ID.
+async function eraseGreeting(elementID)
+{
+    //Keep track of the current text of the element.
+    let currentMessage = elementID.innerText;
+    //Keep track of the length of the element.
+    let upperBound = elementID.innerText.length;
+
+    //While there are letters to hide,
+    for (let i = upperBound; i>=0; i--)
+    {
+        //Hide a letter from the end of the element's text.
+        elementID.innerText= currentMessage.substr(0, i);
+        //Add an underscore to mimic a cursor moving through the text.
+        elementID.innerText+="_";
+        //Wait for a little bit for the user to be able to see the effect.
+        await sleep(60);
+    }
+    
+    //Call the next step.
+    writeGreeting(elementID);
+}
+
+//Start a method to write a new random greeting.
+async function writeGreeting(elementID)
+{
+    //Start an array to hold all potential greetings.
+    const greetings = [
+
+        "Where are we going from here?",
+        "Take a look at my photography, and tell me what you think!",
+        "I have some projects on my GitHub that you should check out.",
+        "There's a page on my design philosophies, if you want a healthy debate.",
+        "It's great to have you stop by.",
+        "I'm always open to some tips and tricks! Feel free to give me some suggestions!",
+
+                      ];
+    //Choose a new greeting                  
+    newGreetingIndex = Math.floor(Math.random() * greetings.length);
+
+    //For as long as the next greeting matches the current one,
+    while (greetingIndex == newGreetingIndex)
+    {
+        //Choose another one.
+        newGreetingIndex = Math.floor(Math.random() * greetings.length);
+    }
+    greetingIndex = newGreetingIndex;
+    chosenGreeting = greetings[newGreetingIndex];
+
+    //Put the length of the element's text string in it's own identifying variable.
+    upperBound = chosenGreeting.length;
+
+    //Start a for loop to iterate through the text in the chosen greeting.
+    for (let i = 0; i <= upperBound; i++)
+    {
+        //Set the text of the element to the first {i} letters of the chosen greeting.
+        elementID.innerText = chosenGreeting.substr(0,i);
+        //Add an underscore to the element text to give the illusion of a cursor writing the message.
+        elementID.innerText+="_";
+        //Wait so the user can see this effect going on.
+        await sleep(50);
+    }
+    //Make sure the greeting at the end matches the chosen greeting, in case the iteration malfunctioned.
+    elementID.innerText = chosenGreeting;
+    //Wait for quite some time for the user to read the text.
+    await sleep(10000);
+    //Restart the effect to generate a new greeting.
+    eraseGreeting(elementID);
+}
+
+//End greeting "type" effect-------------------------------------------------------------------
+
+//Start a method to wait a specific amount of milliseconds.
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
